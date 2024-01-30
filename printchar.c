7addae69ac31e14b28b0e327dacf7366c2b0a8b7
AkintolaRichard <princerichard17a@gmail.com>
2024-01-30 19:59:14 +0100

    style: Add missing bracket and indentation

--- printchar.c
+++ printchar.c
@@ -8,8 +8,8 @@
  */
 void _printchar(int *count, va_list list)
 {
-	_putchar(va_arg(list, int);
-			(*count)++;
+	_putchar(va_arg(list, int));
+	(*count)++;
 }
 /**
  * _printstring - prints array of char to the stdout
@@ -20,20 +20,21 @@ void _printchar(int *count, va_list list)
 
 void _printstring(int *count, va_list list)
 {
-char *s;
-int len = 0, counter = 0;
-s = va_arg(list, char *);
-if (!s)
-{
-_printerror(count);
-return;
-}
-while (s[len])
-	len++;
-while (counter < len)
-{
-	_putchar(s[counter]);
-	(*count)++;
-	counter++;
-}
+	char *s;
+	int len = 0, counter = 0;
+
+	s = va_arg(list, char *);
+	if (!s)
+	{
+		_printerror(count);
+		return;
+	}
+	while (s[len])
+		len++;
+	while (counter < len)
+	{
+		_putchar(s[counter]);
+		(*count)++;
+		counter++;
+	}
 }
