c6e48dfd78567a48ed88c2cdf8aaf190d81bfffe
inifavour <iniobongetuk22@gmail.com>
2024-01-30 09:11:44 -0800

    putchar file added

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+/**
+ * _putchar - writes char to the stdout
+ * @c: char to be printed
+ *
+ * Return: on success 1
+ * on error,-1 is returned, and erno is set appropriately.
+ */
+
+int_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
